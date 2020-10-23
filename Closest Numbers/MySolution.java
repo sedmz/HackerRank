01608b6bcd99a4aac574fee4a718bb886c501c4f
sedmz <48542327+sedmz@users.noreply.github.com>
2020-10-24 02:44:03 +0900

    Create MySolution.java

---
+++ Closest Numbers/MySolution.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    static int[] closestNumbers(int[] arr) {
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i < arr.length; i++) {
+            min = Math.min(min, arr[i] - arr[i - 1]);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i-1] == min) {
+                list.add(arr[i - 1]);
+                list.add(arr[i]);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = closestNumbers(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
